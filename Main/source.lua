011db94fe67463d5eb870b8d54b9944b745f0be2
SpirituaILuke <lukasduda99@gmail.com>
2025-02-09 00:21:16 +0200

    Uploaded files

---
+++ Main/source.lua
@@ -0,0 +1,12 @@
+local scriptLoadAt = tick();
+local function printf() end;
+
+if (not game:IsLoaded()) then
+    game.Loaded:Wait();
+end;
+
+local library = SharedRequire('Utility/UILibrary.lua');
+local Services = SharedRequire('Utility/Services.lua');
+local toCamelCase = SharedRequire('Utility/toCamelCase.lua');
+
+
