658fb92e5cf5817fda3bf01b6112976238510303
SpirituaILuke <lukasduda99@gmail.com>
2025-02-09 02:12:53 +0200

    analyticsapi

--- Main/source.lua
+++ Main/source.lua
@@ -9,4 +9,8 @@ local library = SharedRequire('Utility/UILibrary.lua')
 local Services = SharedRequire('Utility/Services.lua')
 local toCamelCase = SharedRequire('Utility/toCamelCase.lua')
 
+local ToastNotif = SharedRequire('Classes/ToastNotif.lua');
+local AnalayticsAPI = SharedRequire('Classes/AnalyticsAPI.lua');
+local errorAnalytics = AnalayticsAPI.new(getServerConstant('UA-187309782-1'));
+local Utility = SharedRequire('Utility/Utility.lua');
 
