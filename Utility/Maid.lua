74578131370b50f72dd239c7711bffaf68409aa9
SpirituaILuke <lukasduda99@gmail.com>
2025-02-09 18:46:34 +0200

    dddd

--- Utility/Maid.lua
+++ Utility/Maid.lua
@@ -113,11 +113,7 @@ function Maid:DoCleaning()
 		elseif (Signal.isSignal(taskData)) then
 			taskData:Destroy();
 		elseif typeof(taskData) == tableStr then
-			if taskData.Remove then
-				taskData:Remove()
-			else
-				warn("No Remove method found for this table")
-			end
+			taskData:Remove();
 		elseif (typeof(taskData) == threadStr) then
 			task.cancel(taskData);
 		elseif taskData.Destroy then
